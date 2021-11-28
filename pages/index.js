921239f2a5c5295e0b11faf520b07fa1b9185d6f
kgoyan <kaingoyan@gmail.com>
2021-11-27 18:18:49 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import firebase from "./../firebase"
+
+import Brand from "../components/brand"
+import NavBar from "../components/navbar"
+
+
+function Home() {
+  console.log(firebase)
+  return (
+    <>
+      <NavBar/>
+       <Brand tagline="login options" title="home page"/>
+    </>
+  )
+}
+
+export default Home
